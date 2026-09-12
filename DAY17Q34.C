3177ae62d22b367611fe39e2697cb1f121c01b27
Keshav-Garg2007 <Keshav.32254@stu.upes.ac.in>
2026-09-12 22:53:33 +0530

    Add files via upload

---
+++ DAY17Q34.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, prime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        prime = 0;
+    } else {
+        for (i = 2; i < n; i++) {
+            if (n % i == 0) {
+                prime = 0;
+                break;
+            }
+        }
+    }
+
+    if (prime == 1) {
+        printf("Prime");
+    } else {
+        printf("Not prime");
+    }
+
+    return 0;
+}
