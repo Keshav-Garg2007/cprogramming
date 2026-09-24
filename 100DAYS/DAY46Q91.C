9a8c44aa4b4d08e88de1144d30f2acd6be672b1e
KESHAV GARG <Keshav.32254@stu.upes.ac.in>
2026-09-24 19:51:37 +0530

    Add files via upload

---
+++ 100DAYS/DAY46Q91.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("String after removing vowels: ");
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != 'a' && str[i] != 'e' &&
+            str[i] != 'i' && str[i] != 'o' &&
+            str[i] != 'u' && str[i] != 'A' &&
+            str[i] != 'E' && str[i] != 'I' &&
+            str[i] != 'O' && str[i] != 'U') {
+            
+            printf("%c", str[i]);
+        }
+    }
+
+    return 0;
+}
